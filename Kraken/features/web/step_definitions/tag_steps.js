97e8e9e8d0828b1466c6c7b246a8487194c8cbc4
Luis Orjuela <lhorjuelag@gmail.com>
2023-11-18 22:20:53 -0500

    Se agrega el escenario de eliminar tag

--- Kraken/features/web/step_definitions/tag_steps.js
+++ Kraken/features/web/step_definitions/tag_steps.js
@@ -19,7 +19,24 @@ When('I select the tag name {kraken-string} from the list', async function (name
     await this.tagPage.selectTagFromList(name);
 });
 
+When('I delete the tag', async function () {
+    await this.tagPage.deleteTag();
+});
+
 Then('A tag with the name {kraken-string} should exists on the list', async function (name) {
     const exists = await this.tagPage.isTagInList(name);
     expect(exists).to.be.true;
+});
+
+When('I click on the delete tag button', async function () {
+    await this.tagPage.deleteTag();
+});
+
+When('I confirm the delete dialog', async function () {
+    await this.tagPage.confirmDelete();
+});
+
+Then('A tag with the name {string} should not exists on the list', async function (name) {
+    const exists = await this.tagPage.isTagInList(name);
+    expect(exists).to.be.false;
 });
